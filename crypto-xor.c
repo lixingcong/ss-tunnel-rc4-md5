c24b50dc6c23c5786a53a1e854019df04bf2393e
lixingcong <lixingcong@live.com>
2024-12-02 20:19:39 +0800

    WIP

--- crypto-xor.c
+++ crypto-xor.c
@@ -24,41 +24,22 @@
 #include "common.h"
 #include "jhash.h"
 #include "utils.h"
+#include <string.h>
 
-#define CRYPTO_MAX_KEY_SIZE 32
-
-static char crypto_key[CRYPTO_MAX_KEY_SIZE];
+#define XOR_KEY_SIZE 32
+#define XOR_NONCE_SIZE 4
 
 /* An arbitrary initial parameter */
 #define JHASH_INITVAL 0xdeadbeef
 
-#ifdef XXXX // 2024年12月02日 17:09:24
-void crypto_xor_init(const char* password)
+static inline void xor_encrypt_impl(const void* in, void* out, size_t dataLen, const uint8_t* key, size_t keyLen)
 {
-	uint32_t     loopCount      = CRYPTO_MAX_KEY_SIZE / sizeof(uint32_t);
-	uint32_t*    pOut32         = crypto_key;
-	uint32_t     hashVal        = JHASH_INITVAL;
-	const uint32_t passwordLength = min(strlen(password), CRYPTO_MAX_KEY_SIZE);
-
-	memset(crypto_key, 0xff, sizeof(crypto_key));
-	memcpy(crypto_key, password, passwordLength);
-
-	while (loopCount--) {
-		*pOut32 = jhash_1word(*pOut32, hashVal);
-		++pOut32;
-		++hashVal;
-	}
-}
-
-void crypto_xor_encrypt(const void* in, void* out, uint32_t* dlen)
-{
-	uint32_t               i    = 0;
+	size_t               i    = 0;
 	const unsigned char* pIn  = in;
 	unsigned char*       pOut = out;
-	for (; i < *dlen; ++i)
-		*(pOut++) = *(pIn++) ^ crypto_key[i % CRYPTO_MAX_KEY_SIZE];
+	for (; i < dataLen; ++i)
+		*(pOut++) = *(pIn++) ^ key[i % keyLen];
 }
-#endif
 
 void xor_cipher_ctx_init(cipher_ctx_t *ctx, int method, int enc) {}
 
@@ -73,6 +54,27 @@ void xor_ctx_release(cipher_ctx_t *cipher_ctx)
 
 int xor_encrypt_all(buffer_t *plaintext, cipher_t *cipher, size_t capacity)
 {
+	cipher_ctx_t cipher_ctx;
+	xor_ctx_init(cipher, &cipher_ctx, 1);
+
+	static buffer_t tmp = {0, 0, 0, NULL}; // static
+
+	const size_t nonce_len = cipher->nonce_len;
+	const size_t data_len  = plaintext->len;
+
+	brealloc(&tmp, nonce_len + data_len, capacity);
+	buffer_t* ciphertext = &tmp;
+	ciphertext->len      = nonce_len + data_len;
+
+	memcpy(ciphertext->data, cipher_ctx.nonce, nonce_len);
+	xor_encrypt_impl(plaintext->data, ciphertext->data + nonce_len, data_len, cipher->key, cipher->key_len);
+
+	xor_ctx_release(&cipher_ctx);
+
+	brealloc(plaintext, ciphertext->len, capacity);
+	memcpy(plaintext->data, ciphertext->data, ciphertext->len);
+	plaintext->len = ciphertext->len;
+
 	return CRYPTO_OK;
 }
 
@@ -91,8 +93,43 @@ int xor_decrypt(buffer_t *ciphertext, cipher_ctx_t *cipher_ctx, size_t capacity)
 	return CRYPTO_OK;
 }
 
-void xor_ctx_init(cipher_t *cipher, cipher_ctx_t *cipher_ctx, int enc) {}
+void xor_ctx_init(cipher_t* cipher, cipher_ctx_t* cipher_ctx, int enc)
+{
+	memset(cipher_ctx, 0, sizeof(cipher_ctx_t));
+	cipher_ctx->cipher = cipher;
 
-cipher_t *xor_key_init(int method, const char *pass, const char *key) {}
+	if (enc) {
+		rand_bytes(cipher_ctx->nonce, cipher->nonce_len);
+	}
+}
 
-cipher_t *xor_init(const char *pass, const char *key, const char *method) {}
+cipher_t* xor_init(const char* pass, const char* /*key*/, const char* /*method*/)
+{
+	if (!pass || 0 == *pass)
+		return NULL;
+
+	cipher_t* cipher = (cipher_t*) ss_malloc(sizeof(cipher_t));
+	memset(cipher, 0, sizeof(cipher_t));
+
+	cipher->key_len   = XOR_KEY_SIZE;
+	cipher->nonce_len = XOR_NONCE_SIZE;
+
+	{
+		// init the xor key
+		uint32_t       loopCount      = cipher->key_len / sizeof(uint32_t);
+		uint32_t*      pOut32         = cipher->key;
+		uint32_t       hashVal        = JHASH_INITVAL;
+		const uint32_t passwordLength = min(strlen(pass), cipher->key_len);
+
+		memset(cipher->key, 0xff, sizeof(cipher->key));
+		memcpy(cipher->key, pass, passwordLength);
+
+		while (loopCount--) {
+			*pOut32 = jhash_1word(*pOut32, hashVal);
+			++pOut32;
+			++hashVal;
+		}
+	}
+
+	return cipher;
+}
